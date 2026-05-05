6a9bcf1f0ab8d02b1742e34ea6b917b610656fa1
isagthub <isabella.willeke@hotmail.com>
2026-05-05 14:53:50 +0200

    aenderung

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -87,8 +87,8 @@ export default function ContactPage() {
                           <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/><circle cx="12" cy="10" r="3"/>
                         </svg>
                       </div>
-                      
-                    </div>
+                      <div>
+                      </div>
                   </div>
                 </div>
 
